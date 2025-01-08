e6cdb9e76f844a2fa82e05d50023c61d4ceb5daa
Artur Meyer <artur.meyer@estudante.ufjf.br>
2025-01-08 16:40:23 -0300

    Adicionando jogoFacil

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,28 +1,4 @@
 module Lib
     ( 
-        getPalitosFileiraI,
-        gerarTabuleiro,
-        escolheModoDeJogo
     ) where
     
-import System.Random (randomRIO)
-
-data Tabuleiro = Tabuleiro [Int] deriving (Show)
-
-gerarTabuleiro :: Int -> Int -> IO Tabuleiro
-gerarTabuleiro minF maxF = do
-    randomNumber <- randomRIO (minF, maxF)
-    valores <- mapM (\_ -> randomRIO (1, 7)) [1 .. randomNumber]
-    return (Tabuleiro valores)
-
-getPalitosFileiraI :: Tabuleiro -> Int -> Int
-getPalitosFileiraI (Tabuleiro fileira) i =  fileira !! i
-
-escolheModoDeJogo :: IO String
-escolheModoDeJogo = do
-    putStrLn "Escolha o modo de jogo (F para Fácil, D para Difícil):"
-    modo <- getLine
-    return modo
-
-    
-
